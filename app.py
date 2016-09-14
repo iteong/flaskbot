a42073710f19247dc1aa9517ba8a09bc5a4531bb
Ivan Teong <ivanteongsh@gmail.com>
2016-09-14 22:47:54 +1000

    Update with new send types

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ def webhook():
 
     if data["object"] == "page":
 
-        for entry in data["entry"]:
+        for entry in data["entry"]: # loop over each entry (there may be multiple entries if multiple messages sent at once)
+            
+
+
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
@@ -39,7 +42,33 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "got it, thanks!")
+                    #send_message(sender_id, "got it, thanks!")
+
+                    # If we receive a text message, check to see if it matches any special
+                    # keywords and send back the corresponding example. Otherwise, just echo
+                    # the text we received.
+                    special_keywords = {
+                        "image": send_image,
+                        "gif": send_gif,
+                        "audio": send_audio,
+                        "video": send_video,
+                        "file": send_file,
+                        "button": send_button,
+                        "generic": send_generic,
+                        "receipt": send_receipt,
+                        "quick reply": send_quick_reply,
+                        "read receipt": send_read_receipt,
+                        "typing on": send_typing_on,
+                        "typing off": send_typing_off,
+                        "account linking": send_account_linking
+                    }
+
+                    if message_text in special_keywords:
+                        special_keywords[message_text](sender_id) # activate the function
+                    else:
+                        send_message(sender_id, "got it, thanks!")
+                        #page.send(recipient_id, message_text, callback=send_text_callback, notification_type=NotificationType.REGULAR)
+
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -76,6 +105,182 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def send_image(recipient_id):
+    log("sending image to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment":{
+            "type":"image",
+            "payload":{
+            "url": "http://media.topman.com/wcsstore/TopMan/images/catalog/TM83U13MNAV_3col_F_1.jpg"
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def send_gif(recipient_id):
+    log("sending gif to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment":{
+            "type":"image",
+            "payload":{
+            "url": "http://cdn.osxdaily.com/wp-content/uploads/2013/07/dancing-banana.gif"
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def send_button(recipient_id):
+    log("sending buttons to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text":"What do you want to do next?",
+                    "buttons":[
+                    {
+                    "type":"web_url",
+                    "url":"https://www.tradesumo.com",
+                    "title":"See TradeSumo"
+                    },
+                    {
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"USER_DEFINED_PAYLOAD"
+                    }
+                    ]
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def send_generic(recipient_id):
+    log("sending generic template to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                      {
+                        "title":"Welcome to Grilled Cheese & Co",
+                        "item_url":"http://www.ilovegrilledcheese.com",
+                        "image_url":"http://www.thelogofactory.com/wp-content/uploads/2015/10/grilled-cheese-co-logo.png",
+                        "subtitle":"One of life's simple pleasures!",
+                        "buttons":[
+                          {
+                            "type":"web_url",
+                            "url":"http://www.ilovegrilledcheese.com",
+                            "title":"View Website"
+                          },
+                          {
+                            "type":"postback",
+                            "title":"Start Chatting",
+                            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_CHATTING"
+                          }              
+                        ]
+                      }
+                    ]
+                }
+            }
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def send_quick_reply(recipient_id):
+    log("sending quick reply to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "text":"Pick a color:",
+            "quick_replies":[
+              {
+                "content_type":"text",
+                "title":"Red",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+              },
+              {
+                "content_type":"text",
+                "title":"Green",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+              }
+            ]
+          }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
