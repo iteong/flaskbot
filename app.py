86f8001fdfed06b6c227574230694a062bdf96e7
Ivan Teong <ivanteongsh@gmail.com>
2016-09-15 00:11:15 +1000

    Fixing in progress

--- app.py
+++ app.py
@@ -37,10 +37,12 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    message_text = message_text.lower() # convert to lower case
+                    if messaging_event.get("message").get("text"):
+                        
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        message_text = message_text.lower() # convert to lower case
 
                     #send_message(sender_id, "got it, thanks!")
 
